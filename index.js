d0dfb9c131320bb1c2ccfddfe04e8852f0ea6de6
Dennis Verbuijs <verbuijs.d@gmail.com>
2021-12-21 22:18:09 +0100

    Start Message handler

--- index.js
+++ index.js
@@ -1,52 +1,24 @@
 require('dotenv').config();
 const tmi = require('tmi.js');
-const settings = require ('settings.json');
 
-// Define configuration options
-const opts = {
-    identity: {
-      username: process.env.TWITCH_BOT_USERNAME,
-      password: process.env.TWITCH_OAUTH_TOKEN
-    },
-    channels: [
-      'el_snorro'
-    ]
-  };
-  
-  // Create a client with our options
-  const client = new tmi.client(opts);
-  
-  // Register our event handlers (defined below)
-  client.on('message', onMessageHandler);
-  client.on('connected', onConnectedHandler);
-  
-  // Connect to Twitch:
+
+  const client = new tmi.Client({
+      connection:{
+          reconnect:true},
+          channels:['el_snorro'],
+          identity:{
+            username: process.env.TWITCH_BOT_USERNAME,
+            password: process.env.TWITCH_OAUTH_TOKEN
+          }    
+  });
   client.connect();
-  
-  // Called every time a message comes in
-  function onMessageHandler (target, context, msg, self) {
-    if (self) { return; } // Ignore messages from the bot
-  
-    // Remove whitespace from chat message
-    const commandName = msg.trim();
-  
-    // If the command is known, let's execute it
-    if (commandName === '!dice') {
-      const num = rollDice();
-      client.say(target, `You rolled a ${num}`);
-      console.log(`* Executed ${commandName} command`);
-    } else {
-      console.log(`* Unknown command ${commandName}`);
-    }
-  }
-  
-  // Function called when the "dice" command is issued
-  function rollDice () {
-    const sides = 6;
-    return Math.floor(Math.random() * sides) + 1;
-  }
-  
-  // Called every time the bot connects to Twitch chat
-  function onConnectedHandler (addr, port) {
-    console.log(`* Connected to ${addr}:${port}`);
-  }
+  client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+	if(message.toLowerCase() === '!hello') {
+		// "@alca, heya!"
+        console.log("hello");
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+});
